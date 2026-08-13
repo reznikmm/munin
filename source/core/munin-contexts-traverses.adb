6cc1f819abfb98a24e14a1173139cb0f66657952
Max Reznik <reznikmm@gmail.com>
2026-08-13 23:26:50 +0300

    Skip generic package body

--- source/core/munin-contexts-traverses.adb
+++ source/core/munin-contexts-traverses.adb
@@ -13,28 +13,58 @@ package body Munin.Contexts.Traverses is
 
    procedure Visit
      (Node   : Libadalang.Analysis.Ada_Node;
-      Action : access
-        procedure (Name : Libadalang.Analysis.Defining_Name));
+      Action : access procedure (Name : Libadalang.Analysis.Defining_Name));
 
    procedure Visit
      (Node   : Libadalang.Analysis.Ada_Node;
-      Action : access
-        procedure (Name : Libadalang.Analysis.Defining_Name))
+      Action : access procedure (Name : Libadalang.Analysis.Defining_Name))
    is
       Kind : constant Libadalang.Common.Ada_Node_Kind_Type :=
-        Libadalang.Analysis.Kind (Node);
+            Node.Kind;
    begin
-      --  Skip routine bodies: declarations inside are not global
-      if Kind in Libadalang.Common.Ada_Subp_Body
-               | Libadalang.Common.Ada_Entry_Body
-               | Libadalang.Common.Ada_Task_Body
+      --  Skip generic templates: only instantiations are traversed
+      if Kind
+         in Libadalang.Common.Ada_Generic_Package_Decl
+          | Libadalang.Common.Ada_Generic_Subp_Decl
       then
          return;
       end if;
 
-      --  Skip generic templates: only instantiations are traversed
-      if Kind in Libadalang.Common.Ada_Generic_Package_Decl
-               | Libadalang.Common.Ada_Generic_Subp_Decl
+      --  Skip bodies of generic templates.
+      --  Instantiations are handled explicitly below.
+      if Kind
+         in Libadalang.Common.Ada_Package_Body
+          | Libadalang.Common.Ada_Subp_Body
+      then
+         begin
+            declare
+               Decl_Part : constant Libadalang.Analysis.Basic_Decl :=
+                 Node.As_Body_Node.P_Decl_Part;
+               Decl_Kind : constant Libadalang.Common.Ada_Node_Kind_Type :=
+                 (if Decl_Part.Is_Null
+                  then Libadalang.Common.Ada_Node_Kind_Type'First
+                  else Decl_Part.Kind);
+            begin
+               if not Decl_Part.Is_Null and Decl_Kind
+                  in Libadalang.Common.Ada_Generic_Package_Decl
+                   | Libadalang.Common.Ada_Generic_Subp_Decl
+                   | Libadalang.Common.Ada_Generic_Package_Internal
+                   | Libadalang.Common.Ada_Generic_Subp_Internal
+               then
+                  return;
+               end if;
+            end;
+         exception
+            when Libadalang.Common.Property_Error =>
+               null;
+         end;
+      end if;
+
+      --  Skip routine bodies: declarations inside are not global
+      if Kind
+         in Libadalang.Common.Ada_Subp_Body
+          | Libadalang.Common.Ada_Entry_Body
+          | Libadalang.Common.Ada_Task_Body
       then
          return;
       end if;
@@ -43,12 +73,11 @@ package body Munin.Contexts.Traverses is
       if Kind = Libadalang.Common.Ada_Generic_Package_Instantiation then
          begin
             declare
-               Inst : constant
-                 Libadalang.Analysis.Generic_Package_Instantiation :=
+               Inst       :
+                 constant Libadalang.Analysis.Generic_Package_Instantiation :=
                    Node.As_Generic_Package_Instantiation;
-               Designated :
-                 constant Libadalang.Analysis.Generic_Decl :=
-                   Inst.P_Designated_Generic_Decl;
+               Designated : constant Libadalang.Analysis.Generic_Decl :=
+                 Inst.P_Designated_Generic_Decl;
             begin
                if not Designated.Is_Null then
                   for Index in 1 .. Designated.Children_Count loop
@@ -89,17 +118,15 @@ package body Munin.Contexts.Traverses is
 
    procedure Each_Library_Level_Name
      (Self   : Munin.Contexts.Context;
-      Action : access
-        procedure (Name : Libadalang.Analysis.Defining_Name))
-   is
+      Action : access procedure (Name : Libadalang.Analysis.Defining_Name)) is
    begin
       for File_Name of Self.Sources loop
          declare
             File_Path : constant String :=
               VSS.Strings.Conversions.To_UTF_8_String (File_Name);
-            Unit : constant Libadalang.Analysis.Analysis_Unit :=
+            Unit      : constant Libadalang.Analysis.Analysis_Unit :=
               Self.Analysis_Context.Get_From_File (File_Path);
-            Root : constant Libadalang.Analysis.Ada_Node := Unit.Root;
+            Root      : constant Libadalang.Analysis.Ada_Node := Unit.Root;
          begin
             if not Root.Is_Null then
                Visit (Root, Action);
